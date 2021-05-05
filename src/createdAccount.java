74990d5fae5292638096a76de2a3b2924f7260a2
Jackson Santos <56548381+jacksuh@users.noreply.github.com>
2021-05-05 11:58:16 -0300

    First class

--- src/createdAccount.java
+++ src/createdAccount.java
@@ -1,7 +1,7 @@
 public class createdAccount {
     public static void main(String[] args) {
         account firstaccount = new account();
-        firstaccount.saldo = 200;
-        System.out.println(firstaccount.saldo);
+        firstaccount.balance = 200;
+        System.out.println(firstaccount.balance);
     }
 }
