c47247fcd3aa2fdc0da06370d82c0b58fa874b29
Jackson Santos <56548381+jacksuh@users.noreply.github.com>
2021-05-11 13:47:30 -0300

    String imutabilidade

---
+++ src/br/com/bytebank/teste/TesteString.java
@@ -0,0 +1,16 @@
+package br.com.bytebank.teste;
+
+import java.util.Locale;
+
+public class TesteString {
+
+    public static void main(String[] args) {
+
+        String nome = "Alura";
+
+        String outra = nome.replace("A", "a");
+
+        System.out.println(nome);
+        System.out.println(outra);
+    }
+}
