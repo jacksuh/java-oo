020b5a487af161eb833c8781a60fb2c4bb017ffd
Jackson Santos <56548381+jacksuh@users.noreply.github.com>
2021-05-12 14:27:06 -0300

    Arrays Reference

--- src/br/com/bytebank/teste/TestArrayReferencias.java
+++ src/br/com/bytebank/teste/TestArrayReferencias.java
@@ -1,20 +1,31 @@
 package br.com.bytebank.teste;
 
+import br.com.bytebank.modelo.Cliente;
+import br.com.bytebank.modelo.Conta;
 import br.com.bytebank.modelo.ContaCorrente;
+import br.com.bytebank.modelo.ContaPoupanca;
 
 public class TestArrayReferencias {
     public static void main(String[] args) {
-        ContaCorrente[] contas = new ContaCorrente[5];
+        //int[] idades = new int[5];
+        Object[] referencias = new Object[5];
 
         ContaCorrente cc1 = new ContaCorrente(22, 11);
-        contas[0] = cc1;
+        referencias[0] = cc1;
 
-        ContaCorrente cc2 = new ContaCorrente(22, 22);
-        contas[1] = cc2;
+        ContaPoupanca cc2 = new ContaPoupanca(22, 22);
+        referencias[1] = cc2;
 
-        System.out.println(contas[1].getNumero());
+        Cliente cliente = new Cliente();
+        referencias[2] = cliente;
 
-        ContaCorrente ref = contas[1];
+        //System.out.println(cc2.getNumero());
+
+        //Object referenciaGenerica = contas[1];
+
+        //System.out.println( referenciaGenerica.getNumero() );
+
+        ContaPoupanca ref = (ContaPoupanca) contas[1]; //type cast
         System.out.println(cc2.getNumero());
         System.out.println(ref.getNumero());
     }
