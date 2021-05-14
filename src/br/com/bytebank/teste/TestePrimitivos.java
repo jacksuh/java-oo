417d27b03923c28056f95b9adabaf392f1ec96a4
Jackson Santos <56548381+jacksuh@users.noreply.github.com>
2021-05-14 11:44:37 -0300

    ArraysList valor Primitivo

---
+++ src/br/com/bytebank/teste/TestePrimitivos.java
@@ -0,0 +1,19 @@
+package br.com.bytebank.teste;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestePrimitivos {
+
+    public static void main(String[] args) {
+        int[] idades = new int[5];
+
+        String[] nomes = new String[5];
+
+        int idade = 29;//Integer
+        Integer idadeRef = new Integer(29);
+        List<Integer> numeros = new ArrayList<>();
+        numeros.add(idade);
+
+    }
+}
