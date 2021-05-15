cd09535b1d432a7ae7f55b9d79c178367c6af7dd
Jackson Santos <56548381+jacksuh@users.noreply.github.com>
2021-05-15 11:33:39 -0300

    Ordenação por String

--- src/br/com/bytebank/teste/TesteComparacao.java
+++ src/br/com/bytebank/teste/TesteComparacao.java
@@ -50,7 +50,7 @@ public class TesteComparacao {
 
         NumeroDaContaComparator comparator = new NumeroDaContaComparator();
         TitularDaContaComparator titularComparator = new TitularDaContaComparator();
-        lista.sort(titularComparator);
+        lista.sort(new TitularDaContaComparator());
 
         System.out.println("---------");
 
@@ -79,14 +79,18 @@ class NumeroDaContaComparator implements Comparator<Conta> {
     @Override
     public int compare(Conta c1, Conta c2) {
 
-        if(c1.getNumero() < c2.getNumero()) {
-            return -1;
-        }
+        return Integer.compare(c1.getNumero(), c2.getNumero());
 
-        if(c1.getNumero() > c2.getNumero()) {
-            return 1;
-        }
-
-        return 0;
+//                        return c1.getNumero() - c2.getNumero();
+//
+//                if(c1.getNumero() < c2.getNumero()) {
+//                    return -1;
+//                }
+//
+//                if(c1.getNumero() > c2.getNumero()) {
+//                    return 1;
+//                }
+//
+//            return 0;
     }
 }
